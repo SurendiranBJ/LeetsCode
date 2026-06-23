f14919995312a2613cf1dde52606f23b7dcd3374
Surendiran <surabalu212@gmail.com>
2026-06-24 03:55:12 +0530

    Time: 27 ms (24.48%), Space: 12.5 MB (67.19%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)==len(t):
+            if Counter(s)==Counter(t):
+                return True
+            else:
+                return False
+        else:
+            return False            
