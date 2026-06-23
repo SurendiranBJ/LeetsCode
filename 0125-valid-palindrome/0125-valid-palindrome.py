91a4beb8c31e7dcb6beb264152b5310316f5faaa
Surendiran <surabalu212@gmail.com>
2026-06-24 04:24:07 +0530

    Time: 12 ms (82.17%), Space: 12.9 MB (63.58%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,23 @@
+import string
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        i=0
+        j=len(s)-1
+        if s==s[::-1]:
+            return True
+        n = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+        while i<=j:
+            if not s[i].isalnum():
+                i+=1
+            elif not s[j].isalnum():
+                j-=1   
+            elif s[i].lower()!=s[j].lower():
+                return False
+            else:
+                i+=1
+                j-=1
+        return True        
