d801bf50598796dc9e1a2d9ba8d73729dcd03830
Surendiran <surabalu212@gmail.com>
2026-06-28 14:44:48 +0530

    Time: 43 ms (42.11%), Space: 31.7 MB (0%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -12,8 +12,7 @@ class Solution(object):
                 idx=arr.index(1)
                 arr[0],arr[idx]=arr[idx],arr[0]
             else:
-                arr[0]=1
-        arr=np.array(arr)        
+                arr[0]=1        
         for j in range(1,len(arr)):
             if abs(arr[j]-arr[j-1])<=1:
                 continue
