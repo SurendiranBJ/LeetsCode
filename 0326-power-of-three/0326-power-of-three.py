d945bf45e2076f5386bc0de65bef9aa972d16b02
Surendiran <surabalu212@gmail.com>
2026-07-08 19:34:36 +0530

    Time: 29 ms (8.43%), Space: 12.4 MB (53.31%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -1,29 +1,18 @@
 class Solution(object):
     key=False
     def sol(self,n,x):
-        global key
-        if n%3!=0:
-            return
-        if n<3:
+        if n%3!=0 or n<3 or 3**x>n:
             return
         if n==3**x:
             self.key=True
-            return
-        if 3**x>n:
             return     
         self.sol(n,x+1)
         return    
     def isPowerOfThree(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        global key
-        self.sol(n,1)
         if n==1:
             return True
+        self.sol(n,1)
         if self.key:
             return True
         else:
             return False    
-        
