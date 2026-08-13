05bb6b35273d19ffa8d18577aad8b49a1c3e2c09
Surendiran <surabalu212@gmail.com>
2026-08-13 05:44:29 +0530

    Time: 27 ms (87.08%), Space: 21 MB (82.69%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isNStraightHand(self, nums: List[int], k: int) -> bool:
+        if len(nums) % k != 0:
+            return False
+
+        c = Counter(nums)
+
+        for x in sorted(c):
+            if c[x] == 0:
+                continue
+
+            count = c[x]
+
+            for i in range(x, x + k):
+                if c[i] < count:
+                    return False
+                c[i] -= count
+
+        return True
