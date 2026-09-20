a19fb3a2a43c02678f5d524140cbd396325a7fcc
Surendiran <surabalu212@gmail.com>
2026-09-20 07:34:23 +0530

    Time: 44 ms (5.17%), Space: 12.3 MB (55.72%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def reverseDegree(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        apl=[chr(i) for i in range(97,123)]
+        apl=apl[::-1]
+        
+        ans=0
+        for i in range(len(s)):
+            idx=apl.index(s[i])+1
+            ans+=((i+1)*idx)
+        return ans    
